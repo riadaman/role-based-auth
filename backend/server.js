9f79ddc7285f08c4df0d1a7a7af5a95f74a98d74
riadaman <riadmohammod2000@gmail.com>
2025-08-01 14:52:26 +0600

    connect mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
 const app = express();
 
 dotenv.config();
 
+connectDB();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Server is running on port ' + port);
