075b11039e049310b4e51a007eb39f6a78edeb8e
riadaman <riadmohammod2000@gmail.com>
2025-08-02 22:16:44 +0600

    feat(middleware)add:verifyTOken middlware

---
+++ backend/middleware/authMiddlware.js
@@ -0,0 +1,19 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid Token" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
